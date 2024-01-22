9227ce781752a04841e91ba4b524195d800d5259
gandres2416 <gandres2416@gmail.com>
2024-01-21 23:51:56 -0600

    starting official library conversion and update

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
+#include <AHRS.h>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <thread>
 
-int main()
+volatile sig_atomic_t sflag = 0;
+
+void handle_sig(int sig)
 {
-    printf("hello, world!\n");
-    return 1;
+    sflag = 1;
+}
+
+
+int main(int argc, char *argv[]) {
+    std::cout << "Program Executing\n";
+    signal(SIGINT, handle_sig);
+
+    AHRS com = AHRS("/dev/ttyACM1");
+
+    printf("Initializing\n\n");
+
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+
+    std::cout << "Pitch  |  Roll  |  Yaw  |  X-Accel  | Y-Accel  |  Z-Accel  |  Time  |" << std::endl;
+
+    while( 1 == 1){
+        std::cout << std::fixed << std::setprecision(2) << com.GetPitch() << "      " << com.GetRoll() << "   " << com.GetYaw() << "     " <<com.GetWorldLinearAccelX() << "     " << com.GetWorldLinearAccelY() << "       " << com.GetWorldLinearAccelZ() << "      " << com.GetLastSensorTimestamp() << "      " << '\r' << std::flush;
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        if(sflag){
+            sflag = 0;
+            com.Close();
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            break;
+        }
+    }
+    printf("\nExit Caught... Closing device.\n");
+
+    return 0;
 }
