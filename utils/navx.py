ef65ac7d538dc376663219252c286f3502a852b0
gandres2416 <gandres2416@gmail.com>
2024-04-01 12:12:05 -0500

    mapping raw acceleration into world frame

--- utils/navx.py
+++ utils/navx.py
@@ -6,17 +6,20 @@ from scipy.spatial.transform import Rotation as R
 import numpy as np
 import time
 import warnings
+from ahrs.filters import Madgwick
+import math
+import imufusion
 
 warnings.simplefilter("ignore", UserWarning)
 
 class angle_filter():
-    def __init__(self, dt):
+    def __init__(self):
         self.f = KalmanFilter (dim_x=2, dim_z=1)
         self.f.x = np.array([[0.],
                              [0.]])
         self.f.H = np.array([[0, 1]])
         self.f.P *= 1
-        self.f.R = .001
+        self.f.R = .1
         self.f.Q = np.identity(2)
         self.prev_t = time.monotonic_ns()
         self.x = 0
@@ -26,11 +29,10 @@ class angle_filter():
         self.prev_t = time.monotonic_ns()
         self.f.F = np.array([[1.,dt],
                              [0.,1.]])
-        self.f.Q = Q_discrete_white_noise(dim=2, dt=dt, var=.01)
+        self.f.Q = Q_discrete_white_noise(dim=2, dt=dt, var=.1)
         self.f.predict()
         self.f.update(z)
         return self.f.x[0, 0]
-        # return self.x
 
 
 class imu:
@@ -43,42 +45,42 @@ class imu:
         self.az = Value('d', 0)
         self.lock = Lock()
         self.kill = Event()
-        grav = self.calibrate(endpoint)
-        Process(target=self.__update__, args=(endpoint, grav)).start()
+        self.calibrated = Value('i', 0)
+        Process(target=self.__update__, args=(endpoint, )).start()
 
-    def calibrate(self, endpoint):
-        ahrs = AHRS(endpoint, SerialDataType.kProcessedData, 200)
-        x = []
-        y = []
-        z = []
-        c_count = 200
-        while len(x) < c_count or len(y) < c_count or len(z) < c_count or np.var(x) > .0001 or np.var(y) > .0001 or np.var(z) > .0001:
-            x.append(ahrs.get_roll())
-            if len(x) > c_count: x.pop(0)
-            y.append(ahrs.get_pitch())
-            if len(y) > c_count: y.pop(0)
-            z.append(ahrs.get_yaw())
-            if len(z) > c_count: z.pop(0)
-            time.sleep(1/200)
-        ahrs.close()
-        return [np.mean(x), np.mean(y), np.mean(z)]
+    def __update__(self, endpoint):
+        ahrs = imufusion.Ahrs()
+        navx = AHRS(endpoint, SerialDataType.kRaw, 200)
+        prev_time = time.monotonic_ns()
+        offset = 350000 # compensate for lost time, adjust per IMU
+        grav_base = np.array([
+            [0],
+            [0],
+            [1]
+        ])
 
+        while ahrs.flags.initialising:
+            ahrs.update_no_magnetometer(
+                np.array([navx.get_raw_gyro_x(), navx.get_raw_gyro_y(), navx.get_raw_gyro_z()]), 
+                np.array([navx.get_raw_accel_x(), navx.get_raw_accel_y(), navx.get_raw_accel_z()]), 
+                (time.monotonic_ns() - prev_time + offset)/1e9)
+            prev_time = time.monotonic_ns()
 
-    def __update__(self, endpoint, grav):
-        ahrs = AHRS(endpoint, SerialDataType.kRaw, 200)
-        gx_filter = angle_filter(1/200)
-        gy_filter = angle_filter(1/200)
-        gz_filter = angle_filter(1/200)
+        self.calibrated.value = 1
+
         while not self.kill.is_set():            
-            with self.lock:
-                self.gx.value = gx_filter.update(ahrs.get_raw_gyro_x()) #- grav[0]
-                self.gy.value = gy_filter.update(ahrs.get_raw_gyro_y()) #- grav[1]
-                self.gz.value = gz_filter.update(ahrs.get_raw_gyro_z())
-                curr_rot = R.from_euler('xyz', [self.gx.value, self.gy.value, self.gz.value], degrees=True).as_matrix()
-                grav_offset = np.dot(curr_rot, np.array([0, 0, 1]))
-                # print()
-                # print(round(self.gx.value, 2), round(self.gy.value, 2), round(self.gz.value, 2))
-                # print(grav_offset)
+            ahrs.update_no_magnetometer(
+                np.array([navx.get_raw_gyro_x(), navx.get_raw_gyro_y(), navx.get_raw_gyro_z()]), 
+                np.array([navx.get_raw_accel_x(), navx.get_raw_accel_y(), navx.get_raw_accel_z()]), 
+                (time.monotonic_ns() - prev_time + offset)/1e9)
+            prev_time = time.monotonic_ns()
+            self.gx.value = ahrs.quaternion.to_euler()[0]
+            self.gy.value = ahrs.quaternion.to_euler()[1]
+            self.gz.value = ahrs.quaternion.to_euler()[2]
+            grav = ahrs.quaternion.to_matrix().T @ grav_base
+            self.ax.value = navx.get_raw_accel_x() - grav[0, 0]
+            self.ay.value = navx.get_raw_accel_y() - grav[1, 0]
+            self.az.value = navx.get_raw_accel_z() - grav[2, 0]
             time.sleep(1/200)
 
     def get_gyro_x(self):
@@ -93,16 +95,40 @@ class imu:
         with self.lock:
             return round(self.gz.value, 2)
         
+    def get_accel_x(self):
+        with self.lock:
+            return round(self.ax.value, 2)
+        
+    def get_accel_y(self):
+        with self.lock:
+            return round(self.ay.value, 2)
+        
+    def get_accel_z(self):
+        with self.lock:
+            return round(self.az.value, 2)
+        
     def zero_gyro_z(self):
         with self.lock:
             self.gz.value = 0
 
+    def is_calibrated(self):
+        with self.lock:
+            if self.calibrated.value == 1:
+                return True
+            return False
+
 
 i = imu("/dev/ttyACM1")
-print('calibrating...')
+print('calibrating')
+while not i.is_calibrated():
+    time.sleep(.05)
+
 while True:
     print()
     print(i.get_gyro_x())
     print(i.get_gyro_y())
     print(i.get_gyro_z())
+    print(i.get_accel_x())
+    print(i.get_accel_y())
+    print(i.get_accel_z())
     time.sleep(.01)
