5b235b739123bf9633bd51e993a6cca2cd17d2ff
gandres2416 <gandres2416@gmail.com>
2024-04-12 15:48:54 -0500

    comparison testing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 from networktables import NetworkTables
 from utils.dwm import dwm1001
-from utils.kalman import Nonlinear
+from utils.kalman import Fusion, UWB, ReducedModel
 from utils.navx import ahrs
 
 NetworkTables.initialize()
@@ -17,7 +17,8 @@ while not imu.is_calibrated():
 prev_imu = time.monotonic()
 prev_dwm = time.monotonic()
 init_pos = dwm.position()
-ekf = Nonlinear(init_pos.px, init_pos.py, init_pos.pz)
+plain_ekf = UWB(init_pos.px, init_pos.py, init_pos.pz)
+fusion_ekf = Fusion(init_pos.px, init_pos.py, init_pos.pz)
 anchors = []
 try:
     while True:
@@ -25,13 +26,19 @@ try:
             anchors = dwm.anchors()
             prev_dwm = time.monotonic()
         if time.monotonic() - prev_imu >= .005:
-            ekf.dwm_update(anchors,
+            plain_ekf.dwm_update(anchors,
+                           imu.get_accel_x() * 9.8, 
+                           imu.get_accel_y() * 9.8, 
+                           imu.get_accel_z() * 9.8)
+            fusion_ekf.dwm_update(anchors,
                            imu.get_accel_x() * 9.8, 
                            imu.get_accel_y() * 9.8, 
                            imu.get_accel_z() * 9.8)
             prev_imu = time.monotonic()
-            nt.putNumber('fuse_x', round(ekf.get_x()[0, 0], 2))
-            nt.putNumber('fuse_y', round(ekf.get_x()[1, 0], 2))
+            nt.putNumber('dwm_x', round(plain_ekf.get_x()[0, 0], 2))
+            nt.putNumber('dwm_y', round(plain_ekf.get_x()[1, 0], 2))
+            nt.putNumber('fuse_x', round(fusion_ekf.get_x()[0, 0], 2))
+            nt.putNumber('fuse_y', round(fusion_ekf.get_x()[1, 0], 2))
             
 except KeyboardInterrupt:
     dwm.close()
--- utils/kalman.py
+++ utils/kalman.py
@@ -3,7 +3,7 @@ import time
 import math
 from utils.dwm import Anchor
 
-class Nonlinear:
+class Fusion:
     class Anchor:
         def __init__(self, x, y, z):
             self.x = x
@@ -70,12 +70,12 @@ class Nonlinear:
             [0, dt, 0],
             [0, 0, dt]
         ])
-        Q = np.identity(3) * .5
+        Q = np.identity(3)
 
-        R = np.identity(len(anchors) + 3) * .197
-        R[-1, -1] = .69
-        R[-2, -2] = .69
-        R[-3, -3] = .69
+        R = np.identity(len(anchors) + 3) * .19
+        R[-1, -1] = .2
+        R[-2, -2] = .2
+        R[-3, -3] = .2
 
         z = []
         for anchor in anchors:
@@ -83,6 +83,108 @@ class Nonlinear:
         z.append([ax])
         z.append([ay])
         z.append([az])
+        z = np.array(z)
+
+        x_k = self.x[0, 0]
+        y_k = self.x[1, 0]
+        z_k = self.x[2, 0]
+        H = []
+        for anchor in anchors:
+            H.append([
+                (((x_k - anchor.px)**2 + (y_k - anchor.py)**2 + (z_k - anchor.pz)**2)**.5) and (x_k - anchor.px)/(((x_k - anchor.px)**2 + (y_k - anchor.py)**2 + (z_k - anchor.pz)**2)**.5) or 0,
+                (((x_k - anchor.px)**2 + (y_k - anchor.py)**2 + (z_k - anchor.pz)**2)**.5) and (y_k - anchor.py)/(((x_k - anchor.px)**2 + (y_k - anchor.py)**2 + (z_k - anchor.pz)**2)**.5) or 0,
+                (((x_k - anchor.px)**2 + (y_k - anchor.py)**2 + (z_k - anchor.pz)**2)**.5) and (z_k - anchor.pz)/(((x_k - anchor.px)**2 + (y_k - anchor.py)**2 + (z_k - anchor.pz)**2)**.5) or 0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ])
+        H.append([0, 0, 0, 0, 0, 0, 1, 0, 0])
+        H.append([0, 0, 0, 0, 0, 0, 0, 1, 0])
+        H.append([0, 0, 0, 0, 0, 0, 0, 0, 1])
+        H = np.array(H)
+        
+        x_prior = F @ self.x
+        P_prior = F @ self.P @ F.T + (G @ Q @ G.T)
+        K = P_prior @ H.T @ np.linalg.inv(H @ P_prior @ H.T + R)
+        self.x = x_prior + K @ (z - z_factory(self.x, anchors))
+        self.prev_z = z
+        self.P = (np.identity(9) - K @ H) @ P_prior
+
+    def get_x(self):
+        return self.x
+
+class UWB:
+    class Anchor:
+        def __init__(self, x, y, z):
+            self.x = x
+            self.y = y
+            self.z = z
+
+    def __init__(self, init_x: float, init_y: float, init_z: float):
+        self.x = np.array([
+            [init_x],
+            [init_y],
+            [init_z],
+            [0],
+            [0],
+            [0],
+            [0],
+            [0],
+            [0]
+        ])
+
+        self.P = np.identity(9)
+        self.prev_imu = time.monotonic()
+        self.prev_dwm = time.monotonic()
+        
+    
+    def dwm_update(self, anchors: list[Anchor], ax, ay, az):
+        def z_factory(x, anchors: list[Anchor]):
+            eax = x[-3, 0]
+            eay = x[-2, 0]
+            eaz = x[-1, 0]
+            z = []
+            for anchor in anchors:
+                z.append([((x[0, 0] - anchor.px)**2 + (x[1, 0] - anchor.py)**2 + (x[2, 0] - anchor.pz)**2)**.5])
+            return np.array(z)
+
+        if len(anchors) == 0:
+            return
+        
+        dt = time.monotonic() - self.prev_dwm
+        self.prev_dwm = time.monotonic()
+
+        F = np.array([
+            [1, 0, 0, dt, 0, 0, (dt**2)/2, 0, 0],
+            [0, 1, 0, 0, dt, 0, 0, (dt**2)/2, 0],
+            [0, 0, 1, 0, 0, dt, 0, 0, (dt**2)/2],
+            [0, 0, 0, 1, 0, 0, dt, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, dt, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, dt],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1]
+        ])
+
+        G = np.array([
+            [(dt**3)/6, 0, 0],
+            [0, (dt**3)/6, 0],
+            [0, 0, (dt**3)/6],
+            [(dt**2)/2, 0, 0],
+            [0, (dt**2)/2, 0],
+            [0, 0, (dt**2)/2],
+            [dt, 0, 0],
+            [0, dt, 0],
+            [0, 0, dt]
+        ])
+        Q = np.identity(3) * .5
+
+        R = np.identity(len(anchors)) * .197
+
+        z = np.array([[anchor.dst] for anchor in anchors])
         z = np.array(z)
 
         x_k = self.x[0, 0]
@@ -101,9 +203,6 @@ class Nonlinear:
                 0,
                 0
             ])
-        H.append([0, 0, 0, 0, 0, 0, 1, 0, 0])
-        H.append([0, 0, 0, 0, 0, 0, 0, 1, 0])
-        H.append([0, 0, 0, 0, 0, 0, 0, 0, 1])
         H = np.array(H)
         
         x_prior = F @ self.x
